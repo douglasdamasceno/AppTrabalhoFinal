15d8f1a7e5b63897a66f570028f337edfbc7de75
douglasdm <douglasmdamasceno@gmail.com>
2019-10-12 21:02:49 -0300

    mapeando a bottom navegation

--- app/src/main/java/com/example/apptrabalhofinal/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/apptrabalhofinal/ui/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.apptrabalhofinal.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,15 +16,17 @@ import com.example.apptrabalhofinal.data.dao.AtividadeDAO;
 import com.example.apptrabalhofinal.data.dao.AtividadeDBMemoriaDAO;
 import com.example.apptrabalhofinal.data.model.Atividade;
 import com.example.apptrabalhofinal.ui.adapter.MinhaAtividadeAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private ListView listViewMinhasAtividades;
     private Toolbar myToolbar;
 
+    private BottomNavigationView navigationView;
     private static AtividadeDBMemoriaDAO meuBanco;
 
     @Override
@@ -31,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listViewMinhasAtividades = findViewById(R.id.lista_view_minhas_atividades);
+
+        navigationView = findViewById(R.id.bottomNav);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
         myToolbar = findViewById(R.id.minhaToolbar);
 
         setSupportActionBar(myToolbar);
@@ -64,5 +72,24 @@ public class MainActivity extends AppCompatActivity {
             atividades.add(atividade);
         }
         return  atividades;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home: {
+                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_add: {
+                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_buscar: {
+                Toast.makeText(this, "busca", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return false;
     }
 }
